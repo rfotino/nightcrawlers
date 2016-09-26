7100dc5c936c3c8ca09d39dd02c9e1eab438e9c4
Robert Fotino <robert.fotino@gmail.com>
2016-09-26 10:39:46 -0700

    Adds fullscreen on click

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,17 @@
 import { Game } from "./game";
 let game = new Game();
+// Make fullscreen on click
+game.view.addEventListener('click', function() {
+  if (game.view.requestFullscreen) {
+    game.view.requestFullscreen();
+  } else if (game.view.webkitRequestFullscreen) {
+    game.view.webkitRequestFullscreen();
+  } else if (game.view.msRequestFullscreen) {
+    game.view.msRequestFullscreen();
+  } else if (game.view.mozRequestFullscreen) {
+    game.view.mozRequestFullscreen();
+  }
+});
+// Add canvas to document and give it the focus
 document.body.appendChild(game.view);
 game.view.focus();
