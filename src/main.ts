5a31b6e391f4feaea2f4bd6ad4582b04f19e62b1
Robert Fotino <robert.fotino@gmail.com>
2016-09-26 10:49:50 -0700

    Adds fullscreen support for mobile

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { Game } from "./game";
 let game = new Game();
-// Make fullscreen on click
-game.view.addEventListener('click', function() {
+// Make fullscreen on click/tap
+function goFullscreen(): void {
   if (game.view.requestFullscreen) {
     game.view.requestFullscreen();
   } else if (game.view.webkitRequestFullscreen) {
     game.view.webkitRequestFullscreen();
-  } else if (game.view.msRequestFullscreen) {
-    game.view.msRequestFullscreen();
-  } else if (game.view.mozRequestFullscreen) {
-    game.view.mozRequestFullscreen();
+  } else if (game.view['msRequestFullscreen']) {
+    game.view['msRequestFullscreen']();
+  } else if (game.view['mozRequestFullscreen']) {
+    game.view['mozRequestFullscreen']();
   }
-});
+}
+game.view.addEventListener('click', goFullscreen);
+game.view.addEventListener('touchend', goFullscreen);
 // Add canvas to document and give it the focus
 document.body.appendChild(game.view);
 game.view.focus();
