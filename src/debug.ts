e0c7c3041b5e252466500f80675944a8cf504204
Robert Fotino <robert.fotino@gmail.com>
2016-09-25 17:40:34 -0700

    Adds day/night percentage to debugger

--- src/debug.ts
+++ src/debug.ts
@@ -9,17 +9,19 @@ export class Debugger extends GameObject {
   public constructor(container: PIXI.Container, visible: boolean = false) {
     super();
     this._text = new PIXI.Text('', {
-      fontFamily: 'Arial',
-      fontSize: '50px',
+      fontFamily: 'Monospace',
+      fontSize: '24px',
+      lineHeight: 28,
       fill: 'white',
     });
+    this._text.position.set(15, 15);
     this._text.visible = visible;
     container.addChild(this._text);
     this._lastTime = Date.now();
     this._frameTimes = [];
   }
 
-  public update(game: Game) {
+  public update(game: Game): void {
     // Get time taken between frames
     let curTime = Date.now();
     this._frameTimes.push(curTime - this._lastTime);
@@ -30,9 +32,13 @@ export class Debugger extends GameObject {
     let frameTimeSum = this._frameTimes.reduce((p, c) => p + c);
     let frameAvg = frameTimeSum / this._frameTimes.length;
     let fps = frameAvg === 0 ? 0 : 1000 / frameAvg;
+    // Show time of day/night
+    let timeType = game.timeKeeper.isDay ? 'Day' : 'Night';
+    let timePercent = game.timeKeeper.time * 100;
     // Update text display
     this._text.text =
       `Theta: ${(game.player.pos.theta / Math.PI).toFixed(2)}π\n` +
-      `FPS: ${fps.toFixed(0)}`;
+      `FPS: ${fps.toFixed(0)}\n` +
+      `${timeType}: ${timePercent.toFixed(0)}%`;
   }
 }
